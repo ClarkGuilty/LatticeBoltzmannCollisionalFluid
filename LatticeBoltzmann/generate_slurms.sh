a2275b522a89c2edd543f3e17be10d51873adf0a
Javier Acevedo <ja.acevedo12@uniandes.edu.co>
2022-12-01 17:36:12 +0059

    forgot to include the folder in the output path

--- LatticeBoltzmann/generate_slurms.sh
+++ LatticeBoltzmann/generate_slurms.sh
@@ -8,7 +8,7 @@ echo "#!/bin/bash" > slurms/execute_$nprocs
 echo "#SBATCH -n $nprocs" >> slurms/execute_$nprocs
 echo "#SBATCH -A phys-743" >> slurms/execute_$nprocs
 echo "#SBATCH --reservation phys-743" >> slurms/execute_$nprocs
-echo "#SBATCH -o out_$nprocs.out" >> slurms/execute_$nprocs
+echo "#SBATCH -o outputs/out_$nprocs.out" >> slurms/execute_$nprocs
 echo "export PATH=/work/scitas-share/julia-1.8.3/bin:\$PATH" >> slurms/execute_$nprocs
 echo "export LD_LIBRARY_PATH=/work/scitas-share/julia-1.8.3/lib:/work/scitas-share/julia-1.8.3/lib64:\$LD_LIBRARY_PATH" >> slurms/execute_$nprocs
 echo "module load curl" >> slurms/execute_$nprocs
